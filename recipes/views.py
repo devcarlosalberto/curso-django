d7e3474e84d1c50a5d6a32f53050e63417759d19
Carlos Alberto <devcarlosalberto@gmail.com>
2022-04-04 00:09:34 -0300

    Alterações aula 27

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(req):
     return render(req, 'recipes/index.html')
-
-
-def contato(req):
-    return HttpResponse('CONTATO')
-
-
-def sobre(req):
-    return HttpResponse('SOBRE')
